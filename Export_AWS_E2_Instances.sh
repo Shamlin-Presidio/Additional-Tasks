c0c2fc701236368fe3c4d5d365bd5d13c711325d
Shamlin <shamlin.learning@gmail.com>
2025-09-03 07:59:31 +0530

    Create Export_AWS_E2_Instances.sh

---
+++ Export_AWS_E2_Instances.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+####################################
+# Author : Shamlin
+# Date   : 3rd Sep 2025
+
+# Version: V1
+# This script reports the AWS usage
+####################################
+set -x
+
+
+echo "Exporting EC2 instance details to ec2-instances.csv..."
+aws ec2 describe instances \
+  --query "Reservations[*].Instances[*].{
+    InstanceId: InstanceId,
+    Name: Tags[?Key=='Name']|[0].Value,
+    InstanceType: InstanceType,
+    State: State.Name,
+    AvailabilityZone: Placement.AvailabilityZone,
+    PrivateIP: PrivateIpAddress,
+    PublicIP: PublicIpAddress
+  }" \
+  --output text | tr '\t' ',' > ec2-instances.csv
